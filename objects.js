d789a27bb5d5c3f7c73fd2f79e62479c63b5105f
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 14:20:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = { Busiswa: 'Midnight Starring' }
 
 function updatePlaylist(obj, name, title) {
   //return Object.assign({}, obj, name, title)
-  obj.name = title
+  obj[name] = title
   return obj
 }
 
